74709ce60173832c96fb9e6fed4fe1583111301c
Clive Cullen <evilc01@hotmail.com>
2016-12-02 16:32:58 +0000

    heroku images bug

--- games/views.py
+++ games/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from models import team
 from models import game
-from django.db.models import Q
+
 
 # Create your views here.
 def head_to_head(request):
@@ -11,7 +11,7 @@ def head_to_head(request):
     teamOne = get_object_or_404(team, Name=teamOneName)
     teamTwo = get_object_or_404(team, Name=teamTwoName)
 
-    games = game.objects.filter(teamA=teamOne.Name)
+    games = game.objects.filter(teamA = teamOne.name,teamB = teamTwo.name)|(teamA = teamTwo.name, teamB =teamOne.name)
 
 
     return render(request, 'home.html', {'teamOne': teamOne,'teamTwo':teamTwo, 'games': games})
