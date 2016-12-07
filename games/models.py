efee79c9eda4b1cef6d0d8389672a0765bff328b
Clive Cullen <evilc01@hotmail.com>
2016-12-07 20:22:53 +0000

    stats display

--- games/models.py
+++ games/models.py
@@ -8,6 +8,8 @@ class team(models.Model):
     Name = models.CharField(max_length=200)
     image = models.ImageField(upload_to="images", blank=True, null=True)
     rank = models.IntegerField()
+    
+
 
     def __unicode__(self):
         return self.Name
@@ -15,14 +17,15 @@ class team(models.Model):
 
 class game(models.Model):
     date = models.DateField(blank=True, null=True)
-    homeTeam = models.TextField()
-    awayTeam = models.TextField()
-    homeScore = models.IntegerField()
-    awayScore = models.IntegerField()
+    host = models.TextField()
+    teamA = models.TextField()
+    teamB = models.TextField()
+    scoreA = models.IntegerField()
+    scoreB = models.IntegerField()
     competition = models.TextField()
 
     def __unicode__(self):
-        return self.homeTeam + " vs " + self.awayTeam + "  " + self.competition
+        return self.host + " vs " + self.teamB + "  " +self.competition
 
 
 class overall(models.Model):
