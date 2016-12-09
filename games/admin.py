3a5fdc6fc47a4a289c88a89670e7df39cdbd7f56
Clive Cullen <evilc01@hotmail.com>
2016-12-09 22:54:00 +0000

    overall

--- games/admin.py
+++ games/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from models import game, team, overall
+from models import game, team#, overall
 # Register your models here.
 admin.site.register(team)
 admin.site.register(game)
-admin.site.register(overall)
+#admin.site.register(overall)
