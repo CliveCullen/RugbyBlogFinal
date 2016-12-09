bcdb3455a6e7bf26843f7ca66d4f029361d12012
Clive Cullen <evilc01@hotmail.com>
2016-12-09 01:32:23 +0000

    overall

--- games/views.py
+++ games/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
-from models import team
-from models import game
+from models import team, game
+from models import overall
 
 
 # Create your views here.
@@ -14,5 +14,9 @@ def head_to_head(request):
     games = game.objects.filter(teamA=teamOne.Name, teamB=teamTwo.Name)
     games2 = game.objects.filter(teamA=teamTwo.Name, teamB=teamOne.Name)
 
-    return render(request, 'home.html', {'teamOne': teamOne, 'teamTwo': teamTwo, 'games': games, 'games2': games2})
+    history = get_object_or_404(overall, name=teamOneName + " " + teamTwoName + " Overall")
+
+    return render(request, 'home.html', {'teamOne': teamOne, 'teamTwo': teamTwo, 'games': games, 'games2': games2, 'overall':history})
+
+
 
