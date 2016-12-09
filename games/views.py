c80cabfb175ada540847720e78c93f81eefdd73d
Clive Cullen <evilc01@hotmail.com>
2016-12-09 00:01:27 +0000

    took out jquery

--- games/views.py
+++ games/views.py
@@ -3,7 +3,6 @@ from models import team
 from models import game
 
 
-
 # Create your views here.
 def head_to_head(request):
     teamOneName = request.GET.get('teamOne').capitalize()
@@ -12,7 +11,8 @@ def head_to_head(request):
     teamOne = get_object_or_404(team, Name=teamOneName)
     teamTwo = get_object_or_404(team, Name=teamTwoName)
 
-    games = game.objects.filter(teamA=teamOne.Name,teamB=teamTwo.Name)
-    games2= game.objects.filter(teamA=teamTwo.Name,teamB=teamOne.Name)
-    return render(request, 'home.html', {'teamOne': teamOne, 'teamTwo': teamTwo, 'games':games, 'games2':games2})
+    games = game.objects.filter(teamA=teamOne.Name, teamB=teamTwo.Name)
+    games2 = game.objects.filter(teamA=teamTwo.Name, teamB=teamOne.Name)
+
+    return render(request, 'home.html', {'teamOne': teamOne, 'teamTwo': teamTwo, 'games': games, 'games2': games2})
 
