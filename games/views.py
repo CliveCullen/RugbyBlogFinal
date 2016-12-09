e066c17b32b354a2c4147ddbab9b530c46b651d9
Clive Cullen <evilc01@hotmail.com>
2016-12-09 00:15:22 +0000

    still not in order

--- games/views.py
+++ games/views.py
@@ -11,8 +11,8 @@ def head_to_head(request):
     teamOne = get_object_or_404(team, Name=teamOneName)
     teamTwo = get_object_or_404(team, Name=teamTwoName)
 
-    games = game.objects.filter(teamA=teamOne.Name, teamB=teamTwo.Name).order_by("-date")
-    games2 = game.objects.filter(teamA=teamTwo.Name, teamB=teamOne.Name).order_by("-date")
+    games = game.objects.filter(teamA=teamOne.Name, teamB=teamTwo.Name)
+    games2 = game.objects.filter(teamA=teamTwo.Name, teamB=teamOne.Name)
 
     return render(request, 'home.html', {'teamOne': teamOne, 'teamTwo': teamTwo, 'games': games, 'games2': games2})
 
