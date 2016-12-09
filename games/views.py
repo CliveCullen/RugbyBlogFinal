ab6b7b48b07802992c7c3f363f4431cbd48a6ac0
Clive Cullen <evilc01@hotmail.com>
2016-12-09 22:52:12 +0000

    overall

--- games/views.py
+++ games/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from models import team, game
-from models import overall
+#from models import overall
 
 
 # Create your views here.
@@ -8,8 +8,8 @@ def head_to_head(request):
     teamOneName = request.GET.get('teamOne').capitalize()
     teamTwoName = request.GET.get('teamTwo').capitalize()
 
-    h1 = get_object_or_404(overall, Name= teamOneName + " " + teamTwoName + " Overall" )
-    h2 = get_object_or_404(overall, Name= teamOneName + " " + teamTwoName + " Overall" )
+#    h1 = get_object_or_404(overall, Name= teamOneName + " " + teamTwoName + " Overall" )
+ #   h2 = get_object_or_404(overall, Name= teamOneName + " " + teamTwoName + " Overall" )
 
 
     teamOne = get_object_or_404(team, Name=teamOneName)
@@ -18,7 +18,7 @@ def head_to_head(request):
     g1 = game.objects.filter(teamA=teamOne.Name, teamB=teamTwo.Name)
     g2 = game.objects.filter(teamA=teamTwo.Name, teamB=teamOne.Name)
 
-    return render(request,'home.html',{'teamOne':teamOne,'teamTwo':teamTwo,'games':g1,'games2':g2,'h1':h1,'h2':h2})
+    return render(request,'home.html',{'teamOne':teamOne,'teamTwo':teamTwo,'games':g1,'games2':g2})
 
 
 
