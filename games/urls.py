18869f0143e70545919a261e84ff808f51dcc4ef
Clive Cullen <evilc01@hotmail.com>
2016-12-09 11:18:46 +0000

    overall

--- games/urls.py
+++ games/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, include
 from games import views as game_views
 urlpatterns = [
     url(r'^head_to_head/$',game_views.head_to_head, name='head_to_head'),
+
 ]
 
--- games/views.py
+++ games/views.py
@@ -8,13 +8,15 @@ def head_to_head(request):
     teamOneName = request.GET.get('teamOne').capitalize()
     teamTwoName = request.GET.get('teamTwo').capitalize()
 
+    h = teamOneName + " " + teamTwoName + " Overall"
+
     teamOne = get_object_or_404(team, Name=teamOneName)
     teamTwo = get_object_or_404(team, Name=teamTwoName)
 
     games = game.objects.filter(teamA=teamOne.Name, teamB=teamTwo.Name)
     games2 = game.objects.filter(teamA=teamTwo.Name, teamB=teamOne.Name)
 
-    return render(request, 'home.html', {'teamOne': teamOne, 'teamTwo': teamTwo, 'games': games, 'games2': games2})
+    return render(request, 'home.html', {'teamOne': teamOne, 'teamTwo': teamTwo, 'games':games, 'games2':games2, 'h':h})
 
 
 
