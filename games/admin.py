9e3adee7e3edceeb55a6d469012083c1f83c63a5
Clive Cullen <evilc01@hotmail.com>
2016-12-09 22:55:40 +0000

    overall

--- games/admin.py
+++ games/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from models import game, team#, overall
+from models import game, team
 # Register your models here.
 admin.site.register(team)
 admin.site.register(game)
-#admin.site.register(overall)
--- games/views.py
+++ games/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from models import team, game
-#from models import overall
+
 
 
 # Create your views here.
@@ -8,8 +8,6 @@ def head_to_head(request):
     teamOneName = request.GET.get('teamOne').capitalize()
     teamTwoName = request.GET.get('teamTwo').capitalize()
 
-#    h1 = get_object_or_404(overall, Name= teamOneName + " " + teamTwoName + " Overall" )
- #   h2 = get_object_or_404(overall, Name= teamOneName + " " + teamTwoName + " Overall" )
 
 
     teamOne = get_object_or_404(team, Name=teamOneName)
