e799fee28cb9e774f3a5f1c0e6346ab7aaf49f39
Clive Cullen <evilc01@hotmail.com>
2016-12-10 00:11:22 +0000

    history take 2

--- games/admin.py
+++ games/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from models import game, team, history
+from models import game, team, History
 # Register your models here.
 admin.site.register(team)
 admin.site.register(game)
-admin.site.register(history)
+admin.site.register(History)
