f9afbb54ac8d60143e6cd2297e115b36690850d6
Clive Cullen <evilc01@hotmail.com>
2016-12-10 00:02:51 +0000

    wtf

--- games/views.py
+++ games/views.py
@@ -13,20 +13,12 @@ def head_to_head(request):
     teamOne = get_object_or_404(team, Name=teamOneName)
     teamTwo = get_object_or_404(team, Name=teamTwoName)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    g1 = game.objects.filter(teamA=teamOne.Name, teamB=teamTwo.Name)
-    g2 = game.objects.filter(teamA=teamTwo.Name, teamB=teamOne.Name)
 
-    return render(request,'home.html',{'teamOne':teamOne,'teamTwo':teamTwo,'games':g1,'games2':g2})
+    games = game.objects.filter(teamA=teamOne.Name, teamB=teamTwo.Name)
+    games2= game.objects.filter(teamA=teamTwo.Name, teamB=teamOne.Name)
 
-=======
-    games = game.objects.filter(teamA=teamOne.Name, teamB=teamTwo.Name).order_by("-date")
-    games2 = game.objects.filter(teamA=teamTwo.Name, teamB=teamOne.Name).order_by("-date")
->>>>>>> parent of e066c17... still not in order
-=======
-    games = game.objects.filter(teamA=teamOne.Name, teamB=teamTwo.Name).order_by("-date")
-    games2 = game.objects.filter(teamA=teamTwo.Name, teamB=teamOne.Name).order_by("-date")
->>>>>>> parent of e066c17... still not in order
+    return render(request,'home.html',{'teamOne':teamOne,'teamTwo':teamTwo,'games':games,'games2':games2})
+
+ 
 
 
