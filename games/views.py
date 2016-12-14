e4953d79166526a5fa7e11a14fdbc227a741a8a9
Clive Cullen <evilc01@hotmail.com>
2016-12-14 00:38:14 +0000

    table order fix date

--- games/views.py
+++ games/views.py
@@ -13,11 +13,7 @@ def head_to_head(request):
     teamOne = get_object_or_404(team, Name=teamOneName)
     teamTwo = get_object_or_404(team, Name=teamTwoName)
 
-
-    #games = game.objects.filter(teamA=teamOne.Name, teamB=teamTwo.Name)
-    #games2= game.objects.filter(teamA=teamTwo.Name, teamB=teamOne.Name)
-
-    games = game.objects.filter(Q(teamA=teamOne.Name, teamB=teamTwo.Name) | Q(teamA=teamTwo.Name, teamB=teamOne.Name))
+    games = game.objects.filter(Q(teamA=teamOne.Name,teamB=teamTwo.Name)|Q(teamA=teamTwo.Name,teamB=teamOne.Name)).order_by('-date')
 
 
     return render(request,'home.html',{'teamOne':teamOne,'teamTwo':teamTwo,'games':games})
