27f32f6800cf99158df9dd66f644822f936fb598
Clive Cullen <evilc01@hotmail.com>
2016-12-19 14:19:56 +0000

    JODIE IS A LEDGEND

--- rugbyblog/settings.py
+++ rugbyblog/settings.py
@@ -69,7 +69,8 @@ WSGI_APPLICATION = 'rugbyblog.wsgi.application'
 
 DATABASES = {
     'default': {
-
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
     }
 }
 CLEAR_DB_URL = os.environ.get("CLEARDB_DATABASE_URL", "")
