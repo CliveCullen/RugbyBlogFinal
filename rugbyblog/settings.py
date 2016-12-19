c198795d90ed5cc0f74533438052aec8b12580d7
Clive Cullen <evilc01@hotmail.com>
2016-12-19 09:58:44 +0000

    default

--- rugbyblog/settings.py
+++ rugbyblog/settings.py
@@ -68,7 +68,7 @@ WSGI_APPLICATION = 'rugbyblog.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'STRICT_ALL_TABLES': {
+    'default': {
      #   'ENGINE': 'django.db.backends.sqlite3',
       #  'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
